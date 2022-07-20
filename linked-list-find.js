6b5219aeb37576d68aa93d3622746d7d45a1061a
Courtney <courtskit@gmail.com>
2022-07-19 23:32:54 -0700

    recursive solution

--- linked-list-find.js
+++ linked-list-find.js
@@ -1,2 +1,21 @@
 // target value found in linked list ?
 
+const findVal = (head, val) => {
+  let current = head;
+  while (current !== null) {
+    if (current.val === val) {
+      return true
+    }
+    current = current.next;
+  }
+  return false;
+}
+
+
+// recursive 
+
+const linkedListFind = (head, val) => {
+  if (head === null) return false;
+  if (head.val === target) return true;
+  return linkedListFind(head.next, val)
+}
---
+++ return-sum-of-num.js
@@ -0,0 +1,7 @@
+//  return sum of every individual number in given number
+
+const SumOfNum = (num) => {
+
+  
+
+}
